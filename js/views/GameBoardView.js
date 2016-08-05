3b8f841f89baace6cd7f92f53785d51ed0e39ffa
markula <markjr@gmail.com>
2016-08-04 21:20:06 -0500

    some logging

--- js/views/GameBoardView.js
+++ js/views/GameBoardView.js
@@ -3,8 +3,16 @@ define(function() {
   var Backbone = require('backbone');
 
   return Backbone.View.extend({
+    events: {
+      'click .cell': 'handleCellClick'
+    },
+
     initialize: function() {
       console.log(this.model.attributes);
+    },
+
+    handleCellClick: function(e) {
+      console.log($(e.currentTarget));
     }
   });
 });

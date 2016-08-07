43cd975be2a152c0c67e5c126c9d82068d5cf7ac
markula <markjr@gmail.com>
2016-08-06 22:07:27 -0500

    remove unused file

--- js/models/Game.js
+++
@@ -1,8 +0,0 @@
-const _ = require('lodash');
-
-let Game = function(options) {
-  this.numRows = _.get(options, 'boardSize', 3);
-  this.numCols = _.get(options , 'boardSize', 3);
-}
-
-module.exports = Game;
